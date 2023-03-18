6de028f4fd31ff4aac7930cf2e5ce9c686e4c6f0
swapanil810 <sharmaswapnil810@gmail.com>
2023-03-18 17:19:32 +0530

    Retrofit Implementation in get api data using kotlin

--- app/src/main/java/com/example/kt_pro_with_mvvm/activity/RetrofitActivity.kt
+++ app/src/main/java/com/example/kt_pro_with_mvvm/activity/RetrofitActivity.kt
@@ -6,11 +6,13 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kt_pro_with_mvvm.R
 import com.example.kt_pro_with_mvvm.databinding.ActivityRetrofitBinding
+import com.example.kt_pro_with_mvvm.models.Data
 import com.example.kt_pro_with_mvvm.models.FeatchSimpleData
 import com.example.kt_pro_with_mvvm.network.RetrofitInstance
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Response
-import javax.security.auth.callback.Callback
+
 
 class RetrofitActivity : AppCompatActivity() {
     lateinit var binding: ActivityRetrofitBinding
@@ -30,24 +32,44 @@ class RetrofitActivity : AppCompatActivity() {
                     response: Response<FeatchSimpleData?>
                 ) {
                     Log.e("Retrofit_onResponse", "" + response.toString())
-
                     if (response.isSuccessful()) {
+// First  way
+//                        val fetchSimpalData: FeatchSimpleData? = response.body()
+//                        val totalCnt: String = response.body()!!.total.toString()
+//                        val totalPge: String = response.body()!!.total_pages.toString()
+//                        val name: String = response.body()!!.data.get(1).name
+//                        Toast.makeText(this@RetrofitActivity, "API $name", Toast.LENGTH_SHORT).show()
+//                        binding.tv.text = name
+// Second way
+
+                        val userList: FeatchSimpleData? = response.body()
+                        val text: String = userList?.total.toString()
+                        val total: String = userList?.per_page.toString()
+                        val totalPages: Int = userList?.total_pages ?: 0
+                        val datumList: List<Data> = (userList?.data ?: Toast.makeText(
+                            applicationContext,
+                            "$text page\n$total total\n$totalPages totalPages\n", Toast.LENGTH_SHORT
+                        ).show()) as List<Data>
+
+                        for (datum in datumList) {
+                            Toast.makeText(
+                                applicationContext,
+                                "id : " + datum.id + " name: " + datum.name + "color " + datum.color + " year: " + datum.year,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+//                        Log.e("Success", Gson().toJson(response.body()))
+//                        Log.e("Retrofit_Response_Body", "" + response.body().toString())
 
 
                     }
 
-
-//                    binding.tv.text = response.message()
                 }
 
                 override fun onFailure(call: Call<FeatchSimpleData?>, t: Throwable) {
                     Log.e("Retrofit_onFailure", "" + t.toString())
-
-                    TODO("Not yet implemented")
-                    Toast.makeText(
-                        this@RetrofitActivity,
-                        "${t.localizedMessage}",
-                        Toast.LENGTH_SHORT
+                    Toast.makeText(this@RetrofitActivity, "${t.localizedMessage}", Toast.LENGTH_SHORT
                     ).show()
                 }
             })
